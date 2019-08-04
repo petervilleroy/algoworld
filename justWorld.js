cf9aa07bb8b559c51ab515e3527b7b6434e36039
Peter Villeroy <peter.villeroy@gmail.com>
2019-08-04 09:13:32 -0400

    first attempt at re-rendering

--- justWorld.js
+++ justWorld.js
@@ -2,7 +2,9 @@ var worldCanvas, worldStage;
 var spriteArray;
 var currentLevel = 3;
 
+//Define the Citizen Prototype as inheriting from createjs.Container
 function Citizen(name, race, gender, wealth, shapex, shapey, shaper) {
+    //call super constructor
     createjs.Container.call(this);
     this.name = name;
     this.race = race;
@@ -23,6 +25,8 @@ function Citizen(name, race, gender, wealth, shapex, shapey, shaper) {
 
 }
 Citizen.prototype = Object.create(createjs.Container.prototype);
+
+//Render function, for drawing the Citizen
 Citizen.prototype.render = function() {
     // Body
     console.log("Citizen: Drawing body at ("+this.shapex+", "+this.shapey+").")
@@ -32,21 +36,18 @@ Citizen.prototype.render = function() {
     this.bodyShape.x = this.shapex;
     this.bodyShape.y = this.shapey+(this.shaper*2.5);
     this.addChild(this.bodyShape);
-    //this.wealth = Math.round(this.wealth*100);
-
+    
     // Now create the Head
     this.headShape = new createjs.Shape();
     console.log("Citizen: Drawing head at ("+this.shapex+", "+this.shapey+").")
     // apply Race
     if(this.race < 1) {
         this.headShape.graphics.beginStroke(this.lightOutline).beginFill(this.lightHead)
-        .drawCircle(0, 0, this.shaper);
-        //this.race = 0;
+        .drawCircle(0, 0, this.shaper);   
     }
     else {
         this.headShape.graphics.beginStroke(this.darkOutline).beginFill(this.darkHead)
         .drawCircle(0, 0, this.shaper);
-        //this.race = 1;
     }
 
     this.headShape.x = this.shapex;
@@ -96,22 +97,16 @@ function init() {//Draw a square on screen.
 };
 
 function populateLevel_3() {
-	var shape, shapex, shapey, shaper, citizen, citizenx, citizeny;
+	var shapex, shapey, shaper, citizen, citizenx, citizeny;
     var worldPopulation = 20;
     var deathToll = 0;
     var movex = 30;
     var movey = 15;
-    var lightOutline = "#D39972";
-    var lightHead = "#F7C19B";
-    var darkOutline = "#6F4F1D";
-    var darkHead = "#876127";
-    var bodyColor = 'orange';
-
+    
     for (var i = 0; i < worldPopulation; i++) {
         // Create and attach Body
-        shape = new createjs.Shape();
-        shapex = 0; //worldCanvas.width * Math.random() | 0;
-        shapey = 0; //worldCanvas.height * Math.random() | 0;
+        shapex = 0; 
+        shapey = 0; 
         shaper = 10;
         shapeRace = 100*Math.random() | 0;
         shapeGender = 100*Math.random() | 0;
@@ -124,61 +119,13 @@ function populateLevel_3() {
         if(shapeGender < maleProportion) {shapeGender = 0} else {shapeGender = 1}
 
         citizen = new Citizen("citizen_"+i, shapeRace, shapeGender, shapeInitWealth, shapex, shapey, shaper);
-/*      
-        // Body
-        console.log("Drawing body at ("+shapex+", "+shapey+").")
-        // begin at PI/2 - initWealth*PI
-        shape.graphics.beginFill(bodyColor).arc(0, 0, shaper*2, (Math.PI/2)-(Math.PI*shapeInitWealth), (Math.PI/2)+(Math.PI*shapeInitWealth), true);
-        shape.x = shapex;
-        shape.y = shapey+(shaper*2.5);
-        shape.name = "body_"+i;
-        citizen.addChild(shape);
-        citizen.wealth = Math.round(shapeInitWealth*100);
-        
-        // Now create the Head
-        shape = new createjs.Shape();
-        console.log("Drawing head at ("+shapex+", "+shapey+").")
-        // apply Race
-        if(shapeRace < lightProportion) {
-            shape.graphics.beginStroke(lightOutline).beginFill(lightHead).drawCircle(0, 0, shaper);
-            citizen.race = 0;
-        }
-        else {
-            shape.graphics.beginStroke(darkOutline).beginFill(darkHead).drawCircle(0, 0, shaper);
-            citizen.race = 1;
-        }
-        
-        shape.x = shapex;
-        shape.y = shapey;
-        //shape.on("rollover", function (evt) {            this.scale = this.originalScale * 1.2;        });
-        shape.name = "head_"+i;
-        citizen.addChild(shape);
-        
-        // apply Gender
-        shape = new createjs.Shape();
-        console.log("Drawing bow at ("+shapex+", "+shapey+").")
-        if(shapeGender < maleProportion) {
-            citizen.gender = 0;
-        }
-        else {
-            //Draw the female icon
-            shape.graphics.moveTo(-.9*shaper,0);
-            shape.graphics.beginStroke('red').lineTo(-.9*shaper,0).lineTo(-.5*shaper, -1.4*shaper).lineTo(0,-0.9*shaper).lineTo(-1.4*shaper,-0.5*shaper).lineTo(-.9*shaper,0);
-            shape.graphics.beginFill('red').drawCircle(-.7*shaper,-.7*shaper,.2*shaper);
-            citizen.gender = 1;
-        }
-        shape.x = shapex;
-        shape.y = shapey;
-        shape.name = "bow_"+i;
-        citizen.addChild(shape);
-*/
-        // Add the new Citizen to the population
-        //citizen.name = "citizen_"+i;
         citizen.render();
+        
+        // Add the new Citizen to the population
+                
         spriteArray.push(citizen);
         worldStage.addChild(citizen);
-        //spriteArray.forEach(function(sprite, i){worldStage.addChild(sprite);
-        //    console.log("DEBUG: Added "+sprite.name+" to World Stage.")});
+        
     }
     spriteArray.forEach(function(citizen, i){createjs.Tween.get(citizen).to({x: movex, y: movey}, 1500)
     .call(function(citizen){console.log("DEBUG: citizen is now at ("+this.x+","+this.y+")");})});
@@ -190,6 +137,7 @@ function populateLevel_3() {
         spriteArray.forEach (function(citizen, i){
             // Decrement Wealth
             citizen.wealth += 5;
+            citizen.render();
             // Determine mortality and move accordingly
             if(citizen.wealth > 85){
                 citizenx = worldCanvas.width - 25*deathToll;
