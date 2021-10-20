5b07f851805c29d668f2717419ee4bb46821c073
petervilleroy <peter.villeroy@gmail.com>
2021-10-20 09:47:17 +0200

    Fixing speed display. Slider still works backwords

--- justWorld.js
+++ justWorld.js
@@ -214,7 +214,7 @@ function populateLevel_3() {
     
     $("#speedSlider").mouseup(function() {
         tweenSpeed = this.value;
-        $("#currentSpeedLabel").innerHTML = this.value;
+        $("#currentSpeedLabel").innerHTML = this.value.toString();
     })
     //Find more about handling checkbox events at this useful entry: https://stackoverflow.com/questions/3442322/jquery-checkbox-event-handling
 
