cd82cba63360860c2f9cd365fe9cf70f1bf63e2b
Peter Villeroy <peter.villeroy@gmail.com>
2022-02-18 04:30:59 -0800

    fixed new user page

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   resources :useractions, only: [:create, :index, :show]
 
   
-  get 'signup', to: 'user#new', as: 'signup'
+  get 'signup', to: 'users#new', as: 'signup'
   get 'login', to: 'sessions#new', as: 'login'
   get 'logout', to: 'sessions#destroy', as: 'logout'
 
